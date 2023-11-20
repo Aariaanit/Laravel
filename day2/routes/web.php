eccb46c6d62fd7bbb58f87eb48e9414724740347
Aariaanit <arianittershnjaku@gmail.com>
2023-11-20 18:30:20 +0100

    day2

--- day2/routes/web.php
+++ day2/routes/web.php
@@ -13,6 +13,109 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
+
+
+//CSRF Protection (Cross Site Request Forgery)
+ 
+    // Route::get($uri, $callback);
+
+    // Route::post($uri, $callback);
+
+    // Route::put($uri, $callback);
+
+    // Route::patch($uri, $callback);
+
+    // Route::delete($uri, $callback);
+
+    // Route::options($uri, $callback);
+ 
+
+//Perdoret per te gjitha vlerat
+
+Route::match(['get', 'post'], '/login', function () {  
+    return view('login');
+});  
+
+// Route::any('/', function ()   
+// {  
+// // 
+// }); 
+
+
+
+//Laravel Routing Parameters
+
+//Required Parameters
+
+//Home page
+
+Route::get('/', function()  
+{  
+  return "This is a home page";   
+}  
+);  
+//About Page
+Route::get('/about', function()  
+{  
+  return "This is a about page";   
+}  
+);  
+//Contact Page
+Route::get('/contact', function()  
+{  
+  return "This is a contact page";   
+}  
+); 
+
+
+//Example with route parameters
+//Id page
+Route::get('/post/{id}', function($id)  
+{  
+  return "id number is : $id";   
+}  
+); 
+
+
+//Optional Parameters
+//User page
+
+Route::get('user/{name?}', function ($name=null) 
+{  
+    return $name;  
+});  
+
+//User page default
+Route::get('admin/{name?}', function ($name = 'Arianit') 
+{  
+    return $name;  
+});  
+
+//Name page
+Route::get('users/{name?}', function ($name=null) 
+{  
+    return $name;
+    
+})->where('name','[a-zA-ZëË]+');  
+
+
+// //User id
+Route::get('user1/{id?}', function ($id=null) 
+{  
+    return "id is : ". $id;  
+
+})->where('id','[0-9]+');  
+
+
+
+
+
+Route::get('user/{name}/{id}', function ($name,$id) 
+{  
+    return "Id is : ". $id ." , ".  "Name is : ".$name ;  
+    
+})->where(['id'=>'[0-9]+', 'name'=>'[a-zA-Z]+']);  
+
